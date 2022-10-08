b4c63370cd78ea6fac75105d7d96627029d258d4
karkiayush <karkiayush619@gmail.com>
2022-10-08 21:53:05 +0545

    chapter 2 completed & learned about container and text

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,35 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Messenger")),
-      body: Container(
-        child: Center(child: Text("Welcome to messenger")),
+      appBar: AppBar(title: const Text("Messenger")),
+      body: Center(
+        child: Container(
+          alignment: Alignment.center,
+          padding: const EdgeInsets.all(14),
+          // color: Colors.greenAccent,
+          width: 100,
+          height: 100,
+          decoration: const BoxDecoration(
+            color: Colors.greenAccent,
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.grey,
+                  blurRadius: 50,
+                  spreadRadius: 6,
+                  offset: Offset(2.0, 10.0))
+            ],
+            gradient: LinearGradient(colors: [
+              Colors.yellowAccent,
+              Color.fromARGB(255, 102, 97, 98)
+            ]),
+            shape: BoxShape.circle,
+          ),
+
+          child: const Text(
+            "Messenger",
+            style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+          ),
+        ),
       ),
     );
   }
