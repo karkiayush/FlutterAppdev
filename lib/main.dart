3a857ca2fd2aadbb939a76b0f136702f5cc07590
karkiayush <karkiayush619@gmail.com>
2022-10-17 22:06:40 +0545

    Made a simple prototype of programming compiler app using implementing the concepts of the app bar, scaffold, app drawer, list view, user account drawer header and list tile and many more

--- lib/main.dart
+++ lib/main.dart
@@ -197,20 +197,14 @@ class Homepage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          backgroundColor: Color.fromARGB(255, 254, 95, 3),
+          backgroundColor: Colors.green,
           title: Text(
             "Programming Compiler",
             style: TextStyle(
-                color: Color.fromARGB(
-                  255,
-                  248,
-                  248,
-                  30,
-                ),
-                fontFamily: 'Fira'),
+                color: Color.fromARGB(255, 0, 0, 0), fontFamily: 'Fira'),
           )),
       body: Container(
-        color: Colors.teal,
+        color: Colors.white,
         // child:,
       ),
       drawer: Drawer(
@@ -230,15 +224,32 @@ class Homepage extends StatelessWidget {
 
             UserAccountsDrawerHeader(
               decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.all(Radius.circular(30))),
+                color: Colors.blue,
+                borderRadius: BorderRadius.all(Radius.circular(30)),
+              ),
               accountName: Text(
                 "Aayush Karki",
-                style: TextStyle(fontFamily: 'bold1'),
+                style: TextStyle(
+                  fontFamily: 'bold1',
+                ),
               ),
-              accountEmail: Text(
-                "KarkiAayush499@gmail.com",
-                style: TextStyle(fontFamily: 'bold1'),
+              accountEmail: Row(
+                // ignore: prefer_const_literals_to_create_immutables
+                children: [
+                  Text(
+                    "KarkiAayush499@gmail.com",
+                    style: TextStyle(
+                      fontFamily: 'bold1',
+                    ),
+                  ),
+                  // Icon(Icons.edit_outlined)
+                  IconButton(
+                    icon: const Icon(Icons.edit_outlined),
+                    color: Colors.white,
+                    padding: EdgeInsets.only(left: 50),
+                    onPressed: () {},
+                  ),
+                ],
               ),
               currentAccountPicture: CircleAvatar(
                 backgroundImage: NetworkImage(
@@ -246,16 +257,14 @@ class Homepage extends StatelessWidget {
               ),
             ),
 
-            Container(
-              child: ListTile(
-                leading: Icon(Icons.terminal),
-                title: Text(
-                  "Terminal",
-                  style: TextStyle(
-                    fontFamily: 'Fira',
-                    fontSize: 15,
-                    fontWeight: FontWeight.bold,
-                  ),
+            ListTile(
+              leading: Icon(Icons.terminal),
+              title: Text(
+                "Terminal",
+                style: TextStyle(
+                  fontFamily: 'Fira',
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
             ),
@@ -263,6 +272,51 @@ class Homepage extends StatelessWidget {
               leading: Icon(Icons.laptop),
               title: Text(
                 "Editor",
+                style: TextStyle(
+                  fontFamily: 'Fira',
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+
+            ListTile(
+              leading: Icon(Icons.file_copy_outlined),
+              title: Text(
+                "Files",
+                style: TextStyle(
+                  fontFamily: 'Fira',
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.search),
+              title: Text(
+                "Search",
+                style: TextStyle(
+                  fontFamily: 'Fira',
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.account_tree_rounded),
+              title: Text(
+                "Git",
+                style: TextStyle(
+                  fontFamily: 'Fira',
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.extension),
+              title: Text(
+                "Extension",
                 style: TextStyle(
                   fontFamily: 'Fira',
                   fontSize: 15,
@@ -298,6 +352,17 @@ class Homepage extends StatelessWidget {
               leading: Icon(Icons.workspace_premium),
               title: Text(
                 "Premium",
+                style: TextStyle(
+                  fontFamily: 'Fira',
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text(
+                "Setting",
                 style: TextStyle(
                   fontFamily: 'Fira',
                   fontSize: 15,
