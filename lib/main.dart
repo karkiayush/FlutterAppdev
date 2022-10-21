452e59ff9f896ac4537793ae899c613daf3e641d
karkiayush <karkiayush619@gmail.com>
2022-10-21 16:38:58 +0545

    pushed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, unnecessary_string_interpolations
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, unnecessary_string_interpolations, unused_import
 import 'package:flutter/material.dart';
 import 'package:todoapp/drawer.dart';
 import 'package:todoapp/listview.dart';
@@ -16,8 +16,9 @@ class ToDoApp extends StatefulWidget {
 }
 
 class _ToDoAppState extends State<ToDoApp> {
+  late String value;
 //TO MAKE THE DYNAMIC TO DO APP
-  var listToDo = ['Hi, there'];
+  var listToDo = ['', 'code', 'repeat'];
   addToDo(String item) {
     setState(() {
       listToDo.add(item);
@@ -27,32 +28,34 @@ class _ToDoAppState extends State<ToDoApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "To Do App",
-          style: TextStyle(
-              fontFamily: 'cursive1',
-              fontSize: 30,
-              fontWeight: FontWeight.bold),
+        appBar: AppBar(
+          title: Text(
+            "To Do App",
+            style: TextStyle(
+                fontFamily: 'cursive1',
+                fontSize: 30,
+                fontWeight: FontWeight.bold),
+          ),
+          centerTitle: true,
         ),
-        centerTitle: true,
-      ),
-      // body: listView(),
-      body: ListView.builder(
-          itemCount: listToDo.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text('${listToDo[index]}'),
-            );
-          }),
-      drawer: DRAWER(),
-      floatingActionButton: Container(
-        padding: EdgeInsets.all(15),
-        child: FloatingActionButton(
-          onPressed: () {},
-          child: Icon(Icons.add),
-        ),
-      ),
-    );
+        body: ListView.builder(
+            itemCount: listToDo.length,
+            itemBuilder: (context, index) {
+              return index == 0
+                  ? Padding(
+                      padding: const EdgeInsets.all(14.0),
+                      child: TextFormField(),
+                    )
+                  : ListTile(
+                      leading: Icon(Icons.numbers),
+                      title: Text(
+                        '${listToDo[index]}',
+                        style: TextStyle(
+                            fontFamily: 'fira', fontWeight: FontWeight.bold),
+                      ),
+                    );
+            }),
+        floatingActionButton:
+            FloatingActionButton(onPressed: () {}, child: Icon(Icons.add)));
   }
 }
