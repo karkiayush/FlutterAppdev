b72dea1fcbfa3233ab8e9cac6d930f6d8117e32a
karkiayush <karkiayush619@gmail.com>
2022-10-22 00:07:45 +0545

    lot of masala has been added

---
+++ lib/appbar.dart
@@ -0,0 +1,33 @@
+// // ignore_for_file: prefer_const_constructors
+
+// import 'package:flutter/material.dart';
+
+// class appbar extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return AppBar(
+//       backgroundColor: Colors.purple,
+//       centerTitle: true,
+//       title: Row(
+//         // ignore: prefer_const_literals_to_create_immutables
+//         children: [
+//           SizedBox(
+//             width: 120,
+//           ),
+//           Text(
+//             "List To Do",
+//             textAlign: TextAlign.center,
+//             style: TextStyle(
+//               fontFamily: 'cursive1',
+//               fontSize: 30,
+//             ),
+//           ),
+//           SizedBox(
+//             width: 100,
+//           ),
+//           Icon(Icons.dark_mode)
+//         ],
+//       ),
+//     );
+//   }
+// }
